e4c9787679eee78a1476a0fe86598ace86cd4588
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-10-31 23:43:24 +0800

    refactor(meta): use page title instead of route name for current page state

--- composables/useCurrentPage.ts
+++ composables/useCurrentPage.ts
@@ -1,27 +1,18 @@
 import { computed, watch } from "vue";
 
 export const useCurrentPage = () => {
-  const router = useRouter();
-  const pageName = useState('current-page');
-
-  // Format the page name
-  // Remove dashes and capitalize
-  const currentPage = computed(() =>
-    (pageName.value as string)
-      .split('-')
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-      .join(' ')
-  )
+  const route = useRoute();
+  const currentPage = useState('current-page');
 
   const updatePageName = () => {
-    pageName.value = router.currentRoute.value.name || 'Home';
+    currentPage.value = route.meta.title || 'Home';
   }
 
   // Initialize page name
   updatePageName();
 
   // Watch for route changes
-  watch(router.currentRoute, updatePageName);
+  watch(() => route.path, updatePageName);
 
   return { currentPage };
 }
