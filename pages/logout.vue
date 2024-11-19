efb3ee2a81f7cdd27523c8a613ef69afdd28218e
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-11-20 03:41:59 +0800

    new: add logout page

---
+++ pages/logout.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+  const { logout } = useSanctumAuth();
+
+  definePageMeta({
+    title: 'Logout',
+    layout: 'auth',
+    middleware: ['sanctum:auth']
+  })
+
+  onMounted(async () => {
+    await logout()
+  })
+</script>
+
+<template>
+  <div class="auth-container">
+    <div class="card bg-base-200 w-96 shadow-xl">
+      <div class="card-body">
+        <h2 class="text-xl font-bold mb-2">
+          AllerTrac - Logout
+        </h2>
+
+        <p>Please wait while we log you out securely...</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
